988e143a4c16496fb4b0aa93253e7aaf7fdc5485
Bryan Lee <bryanmylee@gmail.com>
2020-09-18 16:20:19 +0800

    Initial commit

---
+++ src/main/java/cs203/g10/ryver/RyverApplication.java
@@ -0,0 +1,11 @@
+package cs203.g10.ryver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RyverApplication.class, args);
+	}
+}
---
+++ src/test/java/cs203/g10/ryver/RyverApplicationTests.java
@@ -0,0 +1,11 @@
+package cs203.g10.ryver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyverApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
