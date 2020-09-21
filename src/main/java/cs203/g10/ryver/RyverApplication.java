6a7cd942d64643c988e0d5c889640001f10846d4
Bryan Lee <bryanmylee@gmail.com>
2020-09-21 22:49:45 +0800

    Set up application context

--- src/main/java/cs203/g10/ryver/RyverApplication.java
+++ src/main/java/cs203/g10/ryver/RyverApplication.java
@@ -2,10 +2,20 @@ package cs203.g10.ryver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import cs203.g10.ryver.user.User;
+import cs203.g10.ryver.user.UserRepository;
 
 @SpringBootApplication
 public class RyverApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RyverApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(RyverApplication.class, args);
+
+        UserRepository userRepo = ctx.getBean(UserRepository.class);
+        BCryptPasswordEncoder encoder = ctx.getBean(BCryptPasswordEncoder.class);
+        System.out.println("[Add user]: " + userRepo.save(
+            new User("admin", encoder.encode("goodpassword"), "ROLE_ADMIN")).getUsername());
 	}
 }
