374cb40a5d1ae44a43f9886f4217fbf18ace09c7
Bryan Lee <bryanmylee@gmail.com>
2020-09-23 00:30:37 +0800

    Integrate Eureka service discovery
    Run the `ryver-auth` service on port `9000`, and added the
    `/service-instances/{appId}` route for service discovery.

--- src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
+++ src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
@@ -2,8 +2,10 @@ package cs203t10.ryver.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RyverBankAuthServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RyverBankAuthServiceApplication.class, args);
