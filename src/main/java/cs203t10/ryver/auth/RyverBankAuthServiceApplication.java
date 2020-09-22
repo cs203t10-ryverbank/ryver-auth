1b05226ef1897021b087b87b246d64b2e774fe8f
Bryan Lee <bryanmylee@gmail.com>
2020-09-22 23:49:22 +0800

    Fix name of main application

--- src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
+++ src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RyverApplication {
+public class RyverBankAuthServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RyverApplication.class, args);
+		SpringApplication.run(RyverBankAuthServiceApplication.class, args);
 	}
 }
