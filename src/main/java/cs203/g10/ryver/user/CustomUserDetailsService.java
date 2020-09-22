a72a29558bce731957d71527c34973561ae737de
Bryan Lee <bryanmylee@gmail.com>
2020-09-22 17:44:52 +0800

    Remove unnecessary comments

--- src/main/java/cs203/g10/ryver/user/CustomUserDetailsService.java
+++ src/main/java/cs203/g10/ryver/user/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package cs203.g10.ryver.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,18 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
     private UserRepository userRepo;
 
-    public CustomUserDetailsService(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
-
     @Override
-    /**
-     * To return a UserDetails for Spring Security
-     * Note that the method takes only a username.
-     * The UserDetails interface has methods to get the password.
-     */
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", username)));
