413a79a7f455497eb4078c79ce849c147465e9b7
Bryan Lee <bryanmylee@gmail.com>
2020-09-23 23:26:46 +0800

    Add root route

---
+++ src/main/java/cs203t10/ryver/auth/RootController.java
@@ -0,0 +1,14 @@
+package cs203t10.ryver.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String getRoot() {
+        return "Welcome to the Ryver Auth Service";
+    }
+
+}
