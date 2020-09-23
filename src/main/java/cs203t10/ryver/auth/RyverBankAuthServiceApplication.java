fcab52effe91bc601d1f3c89731eab20e6e46a7d
Bryan Lee <bryanmylee@gmail.com>
2020-09-24 03:45:21 +0800

    Clean up root route

--- src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
+++ src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
@@ -3,14 +3,22 @@ package cs203t10.ryver.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class RyverBankAuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RyverBankAuthServiceApplication.class, args);
 	}
+
+    @GetMapping("/")
+    public String getRoot() {
+        return "ryver-auth service";
+    }
 
 }
 
