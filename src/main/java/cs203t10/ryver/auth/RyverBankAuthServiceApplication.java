d38919f493ea47573a96beb3a4722b01bb26185b
Bryan Lee <bryanmylee@gmail.com>
2020-09-24 00:34:03 +0800

    Lint source files

--- src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
+++ src/main/java/cs203t10/ryver/auth/RyverBankAuthServiceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RyverBankAuthServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(RyverBankAuthServiceApplication.class, args);
 	}
+
 }
+
